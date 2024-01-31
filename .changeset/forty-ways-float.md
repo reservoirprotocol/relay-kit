cae9c303f0d78a7f98cad4b8b9927cba2ded9b39
armando <armandomvillanueva@icloud.com>
2024-01-31 09:06:36 -0800

    feat: testing

---
+++ .changeset/forty-ways-float.md
@@ -0,0 +1,26 @@
+---
+'@reservoir0x/relay-sdk': patch
+---
+
+Test
+
+
+feat: test hook
+fea : changelo
+feat: version
+feat: remove changeset
+feat: test
+feat: rm slack ping
+feat: remove slack ping
+feat: changeset two
+feat: changeset cmds
+feat: changeset
+feat: rm changeset
+feat: rm readme
+feat: commit
+feat: versio
+feat: disable release
+feat: release commands
+feat: rm
+feat: bun stuff
+feat: changeset libs
---
+++ scripts/generate.js
@@ -0,0 +1,74 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+
+console.log(__dirname)
+
+// Function to append commit messages to the Markdown file
+function appendCommitsToFile(filePath, commitMessages) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err}`)
+      return
+    }
+
+    // Append commit messages to the content
+    const updatedContent = `${data}\n\n${commitMessages}`
+
+    // Write the updated content back to the file
+    fs.writeFile(filePath, updatedContent, 'utf8', (writeErr) => {
+      if (writeErr) {
+        console.error(`Error writing to file: ${writeErr}`)
+      } else {
+        console.log(`Commit messages have been appended to ${filePath}`)
+      }
+    })
+  })
+}
+
+// Function to find the Markdown file in .changesets directory
+function findMarkdownFileAndAppendCommits(commitMessages) {
+  fs.readdir('.changeset', (err, files) => {
+    if (err) {
+      console.error(`Error reading directory: ${err}`)
+      return
+    }
+
+    const mdFile = files.find((file) => file.endsWith('.md'))
+    if (mdFile) {
+      appendCommitsToFile(path.join('.changeset', mdFile), commitMessages)
+    } else {
+      console.error('No Markdown file found in .changesets directory')
+    }
+  })
+}
+
+// Get the current branch name and fetch commits
+exec('git rev-parse --abbrev-ref HEAD', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`)
+    return
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`)
+    return
+  }
+
+  const currentBranch = stdout.trim()
+  exec(
+    `git log main..${currentBranch} --pretty=format:%s`,
+    (logError, logStdout, logStderr) => {
+      if (logError) {
+        console.error(`Error: ${logError.message}`)
+        return
+      }
+      if (logStderr) {
+        console.error(`Stderr: ${logStderr}`)
+        return
+      }
+
+      findMarkdownFileAndAppendCommits(logStdout)
+    }
+  )
+})
