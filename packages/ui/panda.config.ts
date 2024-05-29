d45d99f54b9ee893346dcf892a65923b13072673
pedromcunha <pedro.spinsomewebs@gmail.com>
2024-05-29 18:11:39 -0400

    add pandacss

---
+++ packages/ui/panda.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from '@pandacss/dev'
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    tokens: {
+      spacing: {
+        1: { value: '4px' },
+        2: { value: '8px' },
+        3: { value: '12px' },
+        4: { value: '16px' },
+        5: { value: '32px' },
+        6: { value: '64px' }
+      },
+      fonts: {
+        body: { value: 'var(--font-inter), sans-serif' }
+      },
+      colors: {
+        // Primary
+        primary1: { value: 'green' }
+      }
+    },
+    extend: {}
+  },
+
+  // The output directory for your css system
+  outdir: 'styled-system'
+})
