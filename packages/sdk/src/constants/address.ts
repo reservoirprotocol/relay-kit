6e7cd6bdf083994db10dda1d6ea8d2dc83247b06
Ted Palmer <etpalmer17@gmail.com>
2024-10-30 18:27:38 -0400

    Update solana dead address

--- packages/sdk/src/constants/address.ts
+++ packages/sdk/src/constants/address.ts
@@ -2,7 +2,8 @@ import type { ChainVM } from '../types/RelayChain.js'
 
 export const evmDeadAddress =
   '0x000000000000000000000000000000000000dead' as const
-export const solDeadAddress = '11111111111111111111111111111111' as const //need to check this
+export const solDeadAddress =
+  'CbKGgVKLJFb8bBrf58DnAkdryX6ubewVytn7X957YwNr' as const
 export const bitcoinDeadAddress = 'bc1q4vxn43l44h30nkluqfxd9eckf45vr2awz38lwa'
 
 export const getDeadAddress = (vmType?: ChainVM) => {
