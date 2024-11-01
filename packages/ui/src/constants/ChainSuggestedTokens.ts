9d1dac12834ad1b176095a46dc4b37cb392844b5
Ted Palmer <etpalmer17@gmail.com>
2024-11-01 10:38:20 -0400

    QA feedback

--- packages/ui/src/constants/ChainSuggestedTokens.ts
+++ packages/ui/src/constants/ChainSuggestedTokens.ts
@@ -83,7 +83,8 @@ const ChainSuggestedTokens: Record<number, Token[]> = {
       symbol: 'WETH',
       decimals: 18,
       logoURI:
-        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png'
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
+      verified: true
     },
     {
       chainId: 1,
