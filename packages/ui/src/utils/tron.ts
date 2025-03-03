0249007e0164bfa274b081ac98c2aba462398d6f
pedromcunha <pedro.spinsomewebs@gmail.com>
2025-03-03 08:30:21 -0500

    Add tron chain id

--- packages/ui/src/utils/tron.ts
+++ packages/ui/src/utils/tron.ts
@@ -2,7 +2,7 @@ export const tron = {
   id: 728126428
 }
 
-export const tronAddressRegex = /^(T|3)[a-zA-Z0-9]{34}$/
+export const tronAddressRegex = /^(41[a-fA-F0-9]{40}|T[a-zA-Z0-9]{33})$/
 
 export function isTronAddress(address: string): boolean {
   return tronAddressRegex.test(address)
