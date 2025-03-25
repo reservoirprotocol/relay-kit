88bf2fc4de71c4a303fe2d6f7173fd7d51fc146a
pedromcunha <pedro.spinsomewebs@gmail.com>
2025-03-25 15:49:53 -0400

    Fix build errors

--- demo/pages/ui/singleWallet.tsx
+++ demo/pages/ui/singleWallet.tsx
@@ -1,14 +1,42 @@
 import { NextPage } from 'next'
-import { SwapWidget } from '@reservoir0x/relay-kit-ui'
+import { SwapWidget, Token } from '@reservoir0x/relay-kit-ui'
 import { Layout } from 'components/Layout'
 import { useTheme } from 'next-themes'
 import { useDynamicContext } from '@dynamic-labs/sdk-react-core'
 import { useState } from 'react'
 
 const SingleWalletPage: NextPage = () => {
+  const [singleChainMode, setSingleChainMode] = useState(false)
+  const [toToken, setToToken] = useState<Token | undefined>(
+    singleChainMode
+      ? {
+          chainId: 8453,
+          address: '0x4200000000000000000000000000000000000006',
+          decimals: 18,
+          name: 'WETH',
+          symbol: 'WETH',
+          logoURI:
+            'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png'
+        }
+      : {
+          chainId: 10,
+          address: '0x0000000000000000000000000000000000000000',
+          decimals: 18,
+          name: 'ETH',
+          symbol: 'ETH',
+          logoURI: 'https://assets.relay.link/icons/currencies/eth.png'
+        }
+  )
+  const [fromToken, setFromToken] = useState<Token | undefined>({
+    chainId: 8453,
+    address: '0x0000000000000000000000000000000000000000',
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+    logoURI: 'https://assets.relay.link/icons/currencies/eth.png'
+  })
   const { setShowAuthFlow, primaryWallet } = useDynamicContext()
   const { theme } = useTheme()
-  const [singleChainMode, setSingleChainMode] = useState(false)
 
   return (
     <Layout
@@ -30,44 +58,12 @@ const SingleWalletPage: NextPage = () => {
           lockChainId={singleChainMode ? 8453 : undefined}
           singleChainMode={singleChainMode}
           supportedWalletVMs={['evm', 'bvm', 'svm']}
-          defaultToToken={
-            singleChainMode
-              ? {
-                  chainId: 8453,
-                  address: '0x4200000000000000000000000000000000000006',
-                  decimals: 18,
-                  name: 'WETH',
-                  symbol: 'WETH',
-                  logoURI:
-                    'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png'
-                }
-              : {
-                  chainId: 10,
-                  address: '0x0000000000000000000000000000000000000000',
-                  decimals: 18,
-                  name: 'ETH',
-                  symbol: 'ETH',
-                  logoURI: 'https://assets.relay.link/icons/currencies/eth.png'
-                }
-          }
-          // defaultToToken={{
-          //   chainId: 10,
-          //   address: '0x0000000000000000000000000000000000000000',
-          //   decimals: 18,
-          //   name: 'ETH',
-          //   symbol: 'ETH',
-          //   logoURI: 'https://assets.relay.link/icons/currencies/eth.png'
-          // }}
+          toToken={toToken}
+          setToToken={setToToken}
           // lockToToken={true}
           // lockFromToken={true}
-          defaultFromToken={{
-            chainId: 8453,
-            address: '0x0000000000000000000000000000000000000000',
-            decimals: 18,
-            name: 'ETH',
-            symbol: 'ETH',
-            logoURI: 'https://assets.relay.link/icons/currencies/eth.png'
-          }}
+          fromToken={fromToken}
+          setFromToken={setFromToken}
           // defaultFromToken={{
           //   chainId: 1,
           //   address: '0x446c9033e7516d820cc9a2ce2d0b7328b579406f',
