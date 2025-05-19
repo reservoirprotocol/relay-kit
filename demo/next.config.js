a36a199a0b4cb0c54bcbc9e5c02234acc5cdfdf1
Ted Palmer <etpalmer17@gmail.com>
2025-05-19 14:02:17 -0400

    Add all dynamic packages to optimize imports

--- demo/next.config.js
+++ demo/next.config.js
@@ -6,7 +6,18 @@
 const nextConfig = {
   /* config options here */
   experimental: {
-    optimizePackageImports: ['@dynamic-labs/sdk-react-core', 'viem']
+    optimizePackageImports: [
+      '@dynamic-labs/sdk-react-core',
+      '@dynamic-labs/bitcoin',
+      '@dynamic-labs/eclipse',
+      '@dynamic-labs/ethereum',
+      '@dynamic-labs/ethereum-core',
+      '@dynamic-labs/solana',
+      '@dynamic-labs/sui',
+      '@dynamic-labs/utils',
+      '@dynamic-labs/wagmi-connector',
+      '@dynamic-labs-connectors/abstract-global-wallet-evm'
+    ]
   }
 }
 
