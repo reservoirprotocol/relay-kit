809cf921a0e50f86088551cdd5117c6af6229737
Ted Palmer <etpalmer17@gmail.com>
2025-05-20 09:14:51 -0400

    Try bundling all packages

--- demo/next.config.js
+++ demo/next.config.js
@@ -5,13 +5,7 @@
  */
 const nextConfig = {
   /* config options here */
-  experimental: {
-    optimizePackageImports: [
-      '@dynamic-labs/sdk-react-core',
-      '@reservoir0x/relay-kit-ui',
-      'viem'
-    ]
-  },
+  bundlePagesRouterDependencies: true,
   transpilePackages: [
     '@dynamic-labs/wagmi-connector',
     '@dynamic-labs/sdk-react-core',
