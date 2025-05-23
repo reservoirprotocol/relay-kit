3f247c2f59ecefb8606ab1cd1d193e18609903dc
Ted Palmer <etpalmer17@gmail.com>
2025-05-23 14:20:31 -0400

    Switch back to 0.1s duration for testing

--- packages/ui/src/components/primitives/Dialog.tsx
+++ packages/ui/src/components/primitives/Dialog.tsx
@@ -141,7 +141,7 @@ const AnimatedContent = forwardRef<
         ref={forwardedRef}
         transition={{
           type: isMobile ? 'tween' : undefined,
-          duration: 0.2,
+          duration: 0.1,
           ease: isMobile ? undefined : 'linear'
         }}
         {...animation}
